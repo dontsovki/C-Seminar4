9af4c082d88af16f676ca7e17c4a4d19cb6cd175
Artem.don <dontsovki@gmail.com>
2023-01-14 12:12:33 +0300

    Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int c = GetCountNums(num);
+Console.WriteLine($"Количество цифр = {c}");
+
+// Console.WriteLine($"Количество цифр = {GetCountNums(16)}");
+
+
+int GetCountNums(int number)
+{
+    int count = 0;
+    while(number>0)
+    {
+        count++;
+        number/=10;
+    }
+    return count;
+}
+
+
+
+
+//Console.WriteLine("Введите число: ");
+//string a = Convert.ToString(Console.ReadLine());
+//int i = Convert.ToInt32(a.Length);
+
+//Console.WriteLine($"В числе {a} ровно {i} цифр");
